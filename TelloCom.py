825e8c8c77ca0bf9744af614c4629861f0d69036
Francesco Antoni <francesco.antoni@studenti.galileilivorno.edu.it>
2023-05-02 10:43:03 +0200

    implemented all control commands and set commands

--- TelloCom.py
+++ TelloCom.py
@@ -111,3 +111,37 @@ class TelloCom:
         response, add = self.s.recvfrom(4096)
         return f'From {add.decode("utf-8")}: {response.decode("utf-8")}'
 
+    def go(self, x: int, y: int, z: int, speed: int) -> str:
+        if (x, y, z) not in range(20, 501):
+            raise AttributeError('coordinates not in range(20, 500)')
+        if speed not in range(10, 101):
+            raise AttributeError('speed not in range(10, 101)')
+        command = f'go {x} {y} {z} {speed}'.encode('utf-8')
+        self.s.sendto(command, self.__address_tuple)
+        response, add = self.s.recvfrom(4096)
+        return f'From {add.decode("utf-8")}: {response.decode("utf-8")}'
+
+    def curve(self, x1: int, y1: int, z1: int, x2: int, y2: int, z2: int, speed: int):
+        if (x1, y1, z1, x2, y2, z2) not in range(20, 501):
+            raise AttributeError('coordinates not in range(20, 500)')
+        if speed not in range(10, 101):
+            raise AttributeError('speed not in range(10, 101)')
+        command = f'curve {x1} {y1} {z1} {x2} {y2} {z2} {speed}'.encode('utf-8')
+        self.s.sendto(command, self.__address_tuple)
+        response, add = self.s.recvfrom(4096)
+        return f'From {add.decode("utf-8")}: {response.decode("utf-8")}'
+
+    def set_wifi(self, ssid: str, password: str) -> str:
+        command = f'wifi {ssid} {password}'.encode('utf-8')
+        self.s.sendto(command, self.__address_tuple)
+        response, add = self.s.recvfrom(4096)
+        return f'From {add.decode("utf-8")}: {response.decode("utf-8")}'
+
+    def set_rc(self, a, b, c, d) -> str:
+        if (a, b, c, d) not in range(-100, 100):
+            raise AttributeError('coordinates not in range(-100, 100)')
+        command = f'rc {a} {b} {c} {d}'.encode('utf-8')
+        self.s.sendto(command, self.__address_tuple)
+        response, add = self.s.recvfrom(4096)
+        return f'From {add.decode("utf-8")}: {response.decode("utf-8")}'
+
